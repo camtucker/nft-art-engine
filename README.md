86112f01b28fdcf902552692b90b4bef4188ce25
camtucker <cameronkaufmann@gmail.com>
2024-08-14 10:16:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nft-art-engine
+ Creating layered image output with traits, rarity, and metadata stored in json using Hashlips art engine code.
---
+++ src/config.js
@@ -0,0 +1,126 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "";
+const description = "";
+const baseUri = "ipfs://QmdCp7896komgbbtVGZX5uRwx4uKtARWD94csH1QAJewR6";
+
+const solanaMetadata = {
+  symbol: "CKC",
+  seller_fee_basis_points: 250, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.charmkittycafe.com",
+  creators: [
+    {
+      address: "0x5A507E0061736B8845E061Fded47439f295b6C2E",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10000,
+    layersOrder: [
+      { name: "background" },
+      { name: "ears" },
+      { name: "body" },
+      { name: "face" },
+      { name: "hair" },
+      { name: "scarf" },
+      { name: "tail" },
+      { name: "hat" },
+      { name: "glasses" },
+      { name: "charms" },
+      { name: "accessories" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = true;
+
+const debugLogs = false;
+
+const format = {
+  width: 3000,
+  height: 3000,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 25,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
